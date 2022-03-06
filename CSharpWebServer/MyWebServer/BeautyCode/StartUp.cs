7f669ce8c50a2745a841f8051a58ab1ca1803ac0
Ilcho Dimov <58956165+IDDimov-11@users.noreply.github.com>
2022-03-06 14:25:25 +0200

    Extract separate server and HTTP slasses

---
+++ CSharpWebServer/MyWebServer/BeautyCode/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using MyWebServer.Server;
+using System.Threading.Tasks;
+
+namespace BeautyCode
+{
+    class StartUp
+    {
+        public static async Task Main()
+        {
+            // http://localhost:9090
+
+            var server = new HttpServer("127.0.0.1", 9090);
+
+            await server.Start();
+        }
+    }
+}
